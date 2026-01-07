524d3b1877de8475f1b5b75f481b9ddfb1386e1c
Vince Bongon <vince.jeremyb@gmail.com>
2026-01-07 16:26:56 +0800

    fixing upload api

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,9 @@
 <?php
-
-  require_once './db.php';
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // Adjust for production
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+require_once './db.php';
 
 
 
