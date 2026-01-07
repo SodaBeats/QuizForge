fd37e7ff94f05bf77d2c516f0468229e4108cee0
Vince Bongon <vince.jeremyb@gmail.com>
2026-01-07 15:57:55 +0800

    making file handle and made an upload api

--- QuizForge/src/App.jsx
+++ QuizForge/src/App.jsx
@@ -10,7 +10,7 @@ export default function QuizMakerSkeleton() {
   const [uploadedFile, setUploadedFile] = useState(null);
   const [fileContent, setFileContent] = useState(null);
 
-  const handleFileUpload = (file) => {
+  const async handleFileUpload = (file) => {
     setUploadedFile(file);
 
     const allowedTypes = [
@@ -22,6 +22,19 @@ export default function QuizMakerSkeleton() {
       return;
     }
     setFileContent('Uploading and processing file...');
+
+    const data = new FormData();
+    data.append('file', file);
+
+    await fetch('http://localhost/TESTQUIZFORGE/api/upload.php', {
+      method: 'POST',
+      body: data,
+    });
+
+    const result = await fetch('http://localhost/TESTQUIZFORGE/api/process.php');
+    const text = await result.text();
+    setFileContent(text);
+    
 
   }
 
