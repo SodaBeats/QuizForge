05722c47f991f313842e7ab9b968d7ebd97c786c
Vince Bongon <vince.jeremyb@gmail.com>
2026-01-20 16:07:24 +0800

    removed unneeded code

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,10 +13,6 @@ app.use(express.json());
 
 app.use("/api",healthRoutes);
 
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
 app.listen(3000, () => {
   console.log("API running on http://localhost:3000");
 });
