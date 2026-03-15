e820c31e88de45a81f6db3428fd4d5ad476bab23
Vince Bongon <vince.jeremyb@gmail.com>
2026-03-15 16:08:12 +0800

    Feature(quiz access validator): made a middleware to check user token input

--- backend/src/middlewares/quizAccessValidator.middleware.ts
+++ backend/src/middlewares/quizAccessValidator.middleware.ts
@@ -3,7 +3,8 @@ import type { Request, Response, NextFunction } from 'express';
 import { param, validationResult } from 'express-validator';
 
 export const quizAccessValidator = [
-  param('quizToken').trim().notEmpty().withMessage('Please input a token').isAlphanumeric().toLowerCase(),
+  param('quizToken').trim().notEmpty().withMessage('Please input a token')
+    .isLength({min: 6}).withMessage('Must be a 6 character token').isAlphanumeric().toLowerCase(),
   param('userId').trim().notEmpty().withMessage('You must be logged in').isInt({min: 1}),
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
