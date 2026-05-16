6afc4c8957656dd4cb10f6fa44dadd446ba027a1
Vince Bongon <vince.jeremyb@gmail.com>
2026-05-16 20:28:33 +0800

    chore: renamed middleware

---
+++ backend/src/middlewares/ipBasedRateLimiter.middleware.ts
@@ -0,0 +1,41 @@
+import { redis } from './redis.js';
+import type { Request, Response, NextFunction } from 'express';
+
+export function ipRateLimiter(windowSizeSeconds: number, limit: number) {
+  // lua script for atomicity
+  const luaScript = `
+    local current = redis.call("INCR", KEYS[1])
+    if current == 1 then
+      redis.call("EXPIRE", KEYS[1], ARGV[1])
+    end
+    return current
+  `;
+
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const clientIp = (req.ip ?? 'unknown').replace(/^::ffff:/, '');
+      const clientKey = `ratelimit:${clientIp}`;
+
+      const counter = (await redis.eval(
+        luaScript,
+        1,
+        clientKey,
+        windowSizeSeconds,
+      )) as number;
+
+      if (counter > limit) {
+        return res.status(429).json({
+          success: false,
+          message: 'Too many requests, try again later',
+        });
+      }
+
+      next();
+    } catch (error) {
+      console.error('Inner redis error: ', error);
+      return res
+        .status(500)
+        .json({ success: false, message: 'Redis error, try again later' });
+    }
+  };
+}
