ca363566c6b2affffa97427418b3b838399d6774
Vince Bongon <vince.jeremyb@gmail.com>
2026-05-18 23:13:45 +0800

    feature(tanstack): setup tanstack

--- QuizForge/src/main.jsx
+++ QuizForge/src/main.jsx
@@ -1,16 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './components/AuthProvider.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./components/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
