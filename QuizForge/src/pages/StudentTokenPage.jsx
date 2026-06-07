d222cb2f7f822e061b8753e0176215f903848c52
Vince Bongon <vince.jeremyb@gmail.com>
2026-06-07 20:50:08 +0800

    refactor(StudentQuizPage): separate services and transition from manual state to useQuery

--- QuizForge/src/pages/StudentTokenPage.jsx
+++ QuizForge/src/pages/StudentTokenPage.jsx
@@ -12,36 +12,46 @@ export default function StudentTokenPage() {
   if (!backendHost) throw new Error("Missing backend host");
 
   const handleSubmitToken = async (token) => {
-    const response = await authFetch(`${backendHost}/api/student/quiz-access`, {
-      method: "POST",
-      header: { "Content-Type": "application/json" },
-      body: JSON.stringify({ token }),
-      credentials: "include",
-    });
-
-    if (!response.ok) {
-      const result = await response.json();
-      throw new Error(
-        result.errors?.map((e) => e.msg).join(", ") ||
-          result.message ||
-          "Access Denied",
+    try {
+      const response = await authFetch(
+        `${backendHost}/api/student/quiz-access`,
+        {
+          method: "POST",
+          header: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token }),
+          credentials: "include",
+        },
       );
+
+      if (!response.ok) {
+        const result = await response.json();
+        throw new Error(
+          result.errors?.map((e) => e.msg).join(", ") ||
+            result.message ||
+            "Access Denied",
+        );
+      }
+
+      const quizAndQuestions = await response.json();
+
+      //stop user if already used up all attempts
+      if (quizAndQuestions.totalAttempts >= quizAndQuestions.maxAttempts) {
+        throw new Error(
+          `You have used up all ${quizAndQuestions.maxAttempts} available attempts`,
+        );
+      }
+
+      toast.success("Quiz found! Starting...");
+      setIsModalOpen(false);
+
+      //navigate to quiz page
+      navigate(`/student/quiz/${quizAndQuestions.shareToken}`, {
+        replace: true,
+      });
+    } catch (error) {
+      console.error(error);
+      alert(error.message || "Something went wrong");
     }
-
-    const quizAndQuestions = await response.json();
-
-    //stop user if already used up all attempts
-    if (quizAndQuestions.totalAttempts >= quizAndQuestions.maxAttempts) {
-      throw new Error(
-        `You have used up all ${quizAndQuestions.maxAttempts} available attempts`,
-      );
-    }
-
-    toast.success("Quiz found! Starting...");
-    setIsModalOpen(false);
-
-    //navigate to quiz page
-    navigate(`/student/quiz/${quizAndQuestions.shareToken}`);
   };
 
   return (
