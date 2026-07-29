240b2a953b07a1fdb22d4cd11226b2c0254339e4
Vince Bongon <vince.jeremyb@gmail.com>
2026-07-29 18:17:08 +0800

    chore: minor fix

--- backend/src/middlewares/teacherAuthz.middleware.ts
+++ backend/src/middlewares/teacherAuthz.middleware.ts
@@ -7,8 +7,8 @@ export const verifyTeacher = (
 ) => {
   if (req.user.role !== 'teacher')
     return res
-      .status(401)
-      .json({ success: false, message: 'Unauthorized action' });
+      .status(403)
+      .json({ success: false, message: 'Forbidden action' });
 
   next();
 };
